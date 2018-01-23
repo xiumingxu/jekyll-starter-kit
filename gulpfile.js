bc9a17a47757dc5de9be16671aa93d015f65c87a
vijaykolar <vijayikolar@gmail.com>
2018-01-23 18:09:41 +0530

    Added files for better devlopment using Jekyll, Pug, Sass and Bootstrap 4

---
+++ gulpfile.js
@@ -0,0 +1,94 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync');
+var sass        = require('gulp-sass');
+var prefix      = require('gulp-autoprefixer');
+var cp          = require('child_process');
+var jade        = require('gulp-jade');
+var exec        = require('child_process').exec;
+var gutil       = require('gulp-util');
+
+var messages = {
+    jekyllBuild: 'Connecting'
+};
+
+
+
+
+/**
+ * Build the Jekyll Site
+ */
+gulp.task('build', function (done) {
+    browserSync.notify(messages.jekyllBuild);
+    return cp.spawn('jekyll.bat', ['build'], {stdio: 'inherit'})
+        .on('close', done);
+});
+
+/**
+ * Rebuild Jekyll & do page reload
+ */
+gulp.task('build', ['build'], function () {
+    browserSync.reload();
+});
+
+
+
+
+/**
+ * Wait for build, then launch the Server
+ */
+gulp.task('browser-sync', ['sass', 'build'], function() {
+    browserSync({
+        server: {
+            baseDir: '_site'
+        },
+        notify: false
+    });
+});
+
+
+
+
+/**
+ * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
+ */
+gulp.task('sass', function () {
+    return gulp.src('assets/css/main.scss')
+        .pipe(sass({
+            includePaths: ['css'],
+            onError: browserSync.notify
+        }))
+        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(gulp.dest('_site/assets/css'))
+        .pipe(browserSync.reload({stream:true}))
+        .pipe(gulp.dest('assets/css'));
+});
+
+/*
+* Vijay is trying to Gulp stuff
+*/
+
+gulp.task('jade', function(){
+  return gulp.src('jade/*.jade')
+  .pipe(jade())
+  .pipe(gulp.dest('_includes'));
+});
+
+
+/**
+ * Watch scss files for changes & recompile
+ * Watch html/md files, run jekyll & reload BrowserSync
+ */
+gulp.task('watch', function () {
+    gulp.watch('assets/css/**', ['sass']);
+    gulp.watch(['*.jade', '_layouts/*.html', '_includes/*'], ['build']);
+    gulp.watch('jade/*.jade', ['jade']);
+});
+
+
+
+
+/**
+ * Default task, running just `gulp` will compile the sass,
+ * compile the jekyll site, launch BrowserSync & watch files.
+ */
+gulp.task('default', ['browser-sync', 'watch']);
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Jekyll Starter Kit
+In this project we will learn bunch of new things like sass, jekyll , jade(pug) & gulp etc to use modern way of coding wesites!
+
+Project setup
+
+*	1 Clone this repo.
+*	2 now open bash terminal for project directory and use following command npm i to install packages
+
+        Finally on command line in  project directory use 'gulp --watch' or "gulp" this will open 
+        browser nodejs server at localhost:3000 deploying pages from _site 
+        directory generated in your project folder and when you make changes in 
+        your sass and jade file's they will be reflected in your server site on 
+        save gulp server will also refresh your browser Hope this helps thank 
+        in advance for using my setup!
